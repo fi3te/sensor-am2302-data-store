543f38f6a33ece799d186315e311fae58fc2253a
fi3te <fi3te@users.noreply.github.com>
2023-10-01 15:11:13 +0200

    Updated model

--- pkg/aws/model.go
+++ pkg/aws/model.go
@@ -1,9 +1,9 @@
 package aws
 
 type DataPoint struct {
-	Date        string `dynamodbav:"date" json:"date"`
-	Time        string `dynamodbav:"time" json:"time"`
-	Temperature int64  `dynamodbav:"temperature" json:"temperature"`
-	Humidity    int64  `dynamodbav:"humidity" json:"humidity"`
-	Ttl         int64  `dynamodbav:"ttl" json:"ttl"`
+	Date        string  `dynamodbav:"date" json:"date"`
+	Time        string  `dynamodbav:"time" json:"time"`
+	Temperature float64 `dynamodbav:"temperature" json:"temperature"`
+	Humidity    float64 `dynamodbav:"humidity" json:"humidity"`
+	Ttl         int64   `dynamodbav:"ttl" json:"ttl"`
 }
